6de574574a42b5155ff821ab181b1d3429e52718
Mylaine Mordret <mordret.mylaine@yahoo.fr>
2022-06-27 17:22:39 +0200

    check error

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const numberEl = document.getElementById("number-el")
+const convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function(){
+  console.log("button clicked")
+})
